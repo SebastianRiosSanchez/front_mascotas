570ddafeb9d74f767f649ca53a9d7a74f32b40f3
SebastianRiosSanchez <sebastianrios47@gmail.com>
2024-06-11 18:36:55 -0500

    Avances Registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
+
+  registerForm = this.formBulder.group({
+    nombreCliente: [Validators.required],
+    apellidoCleinte: [Validators.required],
+    direccionCliente: [Validators.required],
+    ccCliente: [Validators.required],
+    emailCliente: [Validators.required, Validators.email],
+    passCliente: [Validators.required],
+  });
+
+  constructor(
+    private formBulder: FormBuilder,
+    private router: Router
+  ) {
+
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+
+  //Registro
+  register() {
+
+    if (this.registerForm.valid) {
+      console.log('Datos del formulario: ', this.register);
+
+    }
+
+  }
+
+
+
 
 }
